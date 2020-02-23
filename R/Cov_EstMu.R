2a02d89023f8408fa96a9af91673a06510dcdd83
Jieli12 <lijie_12@hotmail.com>
2020-02-23 12:02:26 +0000

    change weight to vector

--- R/Cov_EstMu.R
+++ R/Cov_EstMu.R
@@ -11,6 +11,7 @@
 Cov_EstMu <- function(Y, u, u0 = NULL, h, ktype = 'gaussian', poly_order = 0) {
     weight <- kernel_weight(u = u, u0 = u0, bw = h,
                             poly_order = poly_order, ktype = ktype)
+    weight <- as.vector(weight)
     aver <- Y %*% weight
     Cov <- tcrossprod(Y %*% diag(weight), Y) - tcrossprod(aver)
     return(Cov)
