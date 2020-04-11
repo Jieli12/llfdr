93465c588731d1f8ce8740ee8b3e31f72655e8d6
Jieli12 <lijie_12@hotmail.com>
2020-04-12 00:22:38 +0100

    delete space

--- R/Optimise_LMEBeta2.R
+++ R/Optimise_LMEBeta2.R
@@ -9,6 +9,6 @@
 
 Optimise_LMEBeta2 <- function(y2, expu, mat_uk, r0) {
     v <- uniroot(LMEBeta2, c(-100, 100), tol = 1e-6,
-             y2 = y2, expu = expu, mat_uk = mat_uk, r0 = r0)
+            y2 = y2, expu = expu, mat_uk = mat_uk, r0 = r0)
     return(v$root)
 }
