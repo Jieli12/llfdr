1a9c7ecf517b08a91e4c44872fdb723111eb1827
Jieli12 <lijie_12@hotmail.com>
2020-11-24 01:10:44 +0000

    delete repeated header file

--- src/xwxk1.cpp
+++ src/xwxk1.cpp
@@ -1,5 +1,16 @@
-#include <Rcpp.h>
-using namespace Rcpp;
+/***
+* Author         : Jie Li, Ph.D. Candidate, SMSAS, University of Kent.
+* Date           : 2020-02-27 10:57:03
+* Last Revision  : 2020-11-24 01:08:21
+* Last Author    : Jie Li
+* File Path      : /llfdr/src/xwxk1.cpp
+* Description    : delete repeated header file
+*
+*
+*
+* Copyright (c) 2020 by Jie Li, jl705@kent.ac.uk
+* All Rights Reserved.
+*/
 #include <Rcpp.h>
 using namespace Rcpp;
 
@@ -20,17 +31,21 @@ using namespace Rcpp;
 //' @return matrix
 //' @export
 // [[Rcpp::export]]
-NumericMatrix xwxk1(NumericMatrix x, NumericVector w) {
+NumericMatrix xwxk1(NumericMatrix x, NumericVector w)
+{
     int nrow = x.nrow(), ncol = x.ncol();
     NumericMatrix out(nrow, nrow);
 
-    for (int i = 0; i < nrow; i++) {
-        for (int k = 0; k < ncol; k++) {
+    for (int i = 0; i < nrow; i++)
+    {
+        for (int k = 0; k < ncol; k++)
+        {
             // double total = 0;
             double wx = 0;
             wx = w[k] * x(i, k);
             int j;
-            for (j = 0; j < nrow; j++) {
+            for (j = 0; j < nrow; j++)
+            {
                 out(i, j) += wx * x(j, k);
             }
             // out(i, j) *= wx;
@@ -38,7 +53,6 @@ NumericMatrix xwxk1(NumericMatrix x, NumericVector w) {
     }
     return out;
 }
-
 
 // You can include R code blocks in C++ files processed with sourceCpp
 // (useful for testing and development). The R code will be automatically
